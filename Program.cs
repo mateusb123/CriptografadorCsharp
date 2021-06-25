0492bb5e19070f1e0e33f25089c9093871c07c63
mateuzin <pg9779628@gmail.com>
2021-06-24 23:08:22 -0300

    ...

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace criptografador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("msg: ");
+                string msg = Console.ReadLine();
+                msg = msg.Replace(' ', '_');
+                string allCaracters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                int key = 3;
+                string msgCripted = "";
+
+                for (int numberCaracter = 0; numberCaracter < msg.Length; numberCaracter++)
+                {
+                    char caracter = msg[numberCaracter];
+                    if (caracter == '_')
+                    {
+                        msgCripted += ".";
+                    }
+                    else
+                    {
+                        int numberInAllCaracters = allCaracters.IndexOf(caracter);
+                        int newNumberCaracter = numberInAllCaracters + key;
+                        char newCaracter = allCaracters[newNumberCaracter];
+                        msgCripted += newCaracter;
+                    }
+                }
+                Console.WriteLine(msgCripted);
+            }
+        }
+    }
+}
